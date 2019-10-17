5e6ab43b947ae3ca67c0c7e57fae7f785044e8d5
Raghav-B <raghavbhardwaj1998@gmail.com>
2019-10-17 14:22:54 +0800

    Use FXML to create Gui
    This allows us to use the Scene Builder software to easily create the UI!

--- src/main/java/eggventory/ui/Gui.java
+++ src/main/java/eggventory/ui/Gui.java
@@ -1,17 +1,28 @@
 package eggventory.ui;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
 
 public class Gui extends Application {
     @Override
     public void start(Stage stage) {
-        Label helloWorld = new Label("Hello World!"); // Creating a new Label control
-        Scene scene = new Scene(helloWorld); // Setting the scene to be our Label
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(this.getClass().getResource("/Gui.fxml"));
+            //fxmlLoader.setLocation(new URL("file:///D:/App Development/eggventory/src/main/resources/Gui.fxml"));
+            Stage ap = fxmlLoader.load();
 
-        stage.setScene(scene); // Setting the stage to show our screen
-        stage.show(); // Render the stage.
+            //Scene scene = new Scene(ap);
+            //stage.setScene(scene);
+            ap.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
