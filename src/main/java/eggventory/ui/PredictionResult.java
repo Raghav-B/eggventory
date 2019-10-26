37d977e7dbd719d70bb13417e55f3ab5394ef148
Raghav-B <raghavbhardwaj1998@gmail.com>
2019-10-27 01:03:16 +0800

    Add CommandDictionary class

--- src/main/java/eggventory/ui/PredictionResult.java
+++ src/main/java/eggventory/ui/PredictionResult.java
@@ -3,20 +3,11 @@ package eggventory.ui;
 import eggventory.commands.Command;
 
 public interface PredictionResult {
-    public PredictionResult() {
 
-    }
-
-    public String searchCommand(String query) {
+    default void searchCommand(String query) {
         String[] queryArr = query.split(" ");
 
-        int curCommandIndex = queryArr.length;
-        if (curCommandIndex == 1) {
-            for (var x : Command.commandNames.)
-        }
     }
 
     // Structure of array:
-
-
 }
