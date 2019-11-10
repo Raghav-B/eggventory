795d58bbfaa954063d24112f8573b5eaecb4d2c8
Raghav-B <raghavbhardwaj1998@gmail.com>
2019-11-11 03:35:13 +0800

    Add InputPredictor tests

--- src/test/java/eggventory/ui/InputPredictorTest.java
+++ src/test/java/eggventory/ui/InputPredictorTest.java
@@ -1,5 +1,68 @@
 package eggventory.ui;
 
-public class InputPredictorTest {
+import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+//@@author Raghav-B
+/**
+ * Tests if input predictor returns correct predictions for incomplete
+ * inputs.
+ */
+public class InputPredictorTest {
+    private InputPredictor inputPredictor = new InputPredictor();
+
+    @Test
+    public void testGetPrediction_Command_Succeeds() {
+        String incompleteInput = "ad";
+        assertEquals(incompleteInput + inputPredictor.getPrediction(incompleteInput, 0),
+                "add stock");
+
+        incompleteInput = "b";
+        assertEquals(incompleteInput + inputPredictor.getPrediction(incompleteInput, 0),
+                "bye");
+
+        incompleteInput = "add stockt";
+        assertEquals(incompleteInput + inputPredictor.getPrediction(incompleteInput, 0),
+                "add stocktype");
+    }
+
+    @Test
+    public void testGetPrediction_Command_Fails() {
+        String incompleteInput = "udfvjh";
+        String remainString = inputPredictor.getPrediction(incompleteInput, 0);
+
+        assertEquals(remainString, "");
+    }
+
+    @Test
+    void testGetPrediction_Argument_Succeeds() {
+        String incompleteInput = "add stock";
+        assertEquals(inputPredictor.getPrediction(incompleteInput, 0),
+                " <StockType> <StockCode> <Quantity> <Description> [Optional: -m <Minimum quantity>]");
+
+        incompleteInput += " ";
+        System.out.println(inputPredictor.getPrediction(incompleteInput, 0));
+        assertEquals(inputPredictor.getPrediction(incompleteInput, 0),
+                "<StockType> <StockCode> <Quantity> <Description> [Optional: -m <Minimum quantity>] ");
+
+        incompleteInput += "Arduino 398SH ";
+        assertEquals(inputPredictor.getPrediction(incompleteInput, 0),
+                "<Quantity> <Description> [Optional: -m <Minimum quantity>] ");
+
+        inputPredictor.reset();
+        incompleteInput = "edit stock";
+        assertEquals(inputPredictor.getPrediction(incompleteInput, 0),
+                " <StockCode> <Property> <NewValue>");
+
+        incompleteInput += " Arduino";
+        assertEquals(inputPredictor.getPrediction(incompleteInput, 0),
+                " <Property> <NewValue> ");
+
+        incompleteInput += " StockCode SH983";
+        assertEquals(inputPredictor.getPrediction(incompleteInput, 0),
+                " ");
+    }
 }
+//@@author
