7b4f631c7ec0803a06a17ce4d4cd15491844d3de
Raghav-B <raghavbhardwaj1998@gmail.com>
2019-11-11 02:38:26 +0800

    Add/fix some @@author tags

--- src/main/java/eggventory/logic/parsers/Parser.java
+++ src/main/java/eggventory/logic/parsers/Parser.java
@@ -11,8 +11,6 @@ import eggventory.logic.commands.CommandDictionary;
 import java.util.Arrays;
 import java.util.HashSet;
 
-;
-
 //@@author cyanoei
 /**
  * Interprets command strings by the user, and converts them to command objects that can be executed.
@@ -71,8 +69,9 @@ public class Parser {
         String[] commandArr = command.split(" ");
         return commandArr.length - 1 >= reqArguments;
     }
-    //@@author cyanoei
+    //@@author
 
+    //@@author cyanoei
     /**
      * Checks if a string input is an integer.
      * @param testInteger the input to test.
